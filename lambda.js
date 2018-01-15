19fa3c3dbf40beeef39a846f8d82268672b89b0f
Archit1992 <gajjararchit00004@gmail.com>
2018-01-14 20:54:37 -0800

    JIRA WAP-1234 adding sample commit

--- lambda.js
+++ lambda.js
@@ -2,5 +2,5 @@
 const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./app')
 const server = awsServerlessExpress.createServer(app)
- // Archit
+ // Archit here
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
